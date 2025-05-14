ad5a3ac713e63e885033ac9b83e6c284a150adf9
Jarrod Swords <jarrod.swords@gmail.com>
2025-05-14 16:52:35 -0400

    checkpoint

---
+++ builder3/src/Blog/Infrastructure.Read/Database/Comment.cs
@@ -0,0 +1,11 @@
+﻿namespace DesignPatterns.Builder3.Blog.Infrastructure.Read.Database;
+
+public record Comment(
+    uint Id,
+    uint? ParentId,
+    uint PostId,
+    uint RootId,
+    uint UserId,
+    string Text,
+    DateTime Timestamp
+);
