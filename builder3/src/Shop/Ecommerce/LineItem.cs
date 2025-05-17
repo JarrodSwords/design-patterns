473189f6cc1b667d16839228e96c6dae564cc5f4
Jarrod Swords <jarrod.swords@gmail.com>
2025-05-17 17:41:03 -0400

    removed experimental branch

--- builder3/src/Shop/Ecommerce/LineItem.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DesignPatterns.Builder3.Shop.Ecommerce;
-
-public class LineItem
-{
-    public Guid Id { get; }
-    public string Name { get; }
-    public decimal Price { get; }
-    public string Sku { get; }
-}
