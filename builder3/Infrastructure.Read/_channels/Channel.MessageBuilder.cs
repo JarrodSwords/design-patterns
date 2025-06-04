d4e26e00da9956f22bc8497da0328505d6d003f3
Jarrod Swords <jarrod.swords@gmail.com>
2025-06-04 12:06:15 -0400

    cleanup

--- builder3/Infrastructure.Read/_channels/Channel.MessageBuilder.cs
+++ builder3/Infrastructure.Read/_channels/Channel.MessageBuilder.cs
@@ -1,16 +1,16 @@
-﻿namespace Examples.SocialMedia.Infrastructure.Read;
+﻿using Jgs.Errors.Results;
+
+namespace Examples.SocialMedia.Infrastructure.Read;
 
 public partial class Channel : IMessageBuilder
 {
-    public object Add(Database.Message message, Database.User user) =>
+    public Result Add(Database.Message message, Database.User user) =>
         Message.From(message, user)
             .Then(
                 m =>
                 {
                     if (!_messages.ContainsValue(m))
                         _messages.Add(m.Timestamp, m);
-
-                    return m;
                 }
             );
 }
