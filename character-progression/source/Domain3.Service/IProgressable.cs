8ff5c68dad3995f1691d22e82d7f7f26b2ad3292
Jarrod Swords <jarrod.swords@gmail.com>
2025-09-18 04:45:07 -0400

    first pass service

---
+++ character-progression/source/Domain3.Service/IProgressable.cs
@@ -0,0 +1,94 @@
+﻿namespace CharacterProgression.Domain3.Service;
+
+public enum ProgressionType
+{
+    Standard,
+    Boosted
+}
+
+public interface IProgressable
+{
+    public ProgressionType ProgressionType { get; }
+    Xp Xp { get; }
+    void Set(Xp xp);
+}
+
+public abstract class ProgressionRate
+{
+    public abstract void Add(IProgressable progressable, Xp xp, Xp maxXp);
+}
+
+public class StandardRate : ProgressionRate
+{
+    public override void Add(IProgressable progressable, Xp xp, Xp maxXp)
+    {
+        progressable.Set((Xp) Math.Min(progressable.Xp + xp, maxXp));
+    }
+}
+
+public class BoostedRate : ProgressionRate
+{
+    public override void Add(IProgressable progressable, Xp xp, Xp maxXp)
+    {
+        progressable.Set((Xp) Math.Min(progressable.Xp + xp * 2, maxXp));
+    }
+}
+
+public abstract class Accessory
+{
+}
+
+public class ExpBooster : Accessory
+{
+}
+
+public class Character(ProgressionType progressionType = ProgressionType.Standard, Xp? xp = null) : IProgressable
+{
+    public static readonly Xp MaxXp = 9999;
+    private Accessory _accessory;
+
+    public Accessory Accessory
+    {
+        get => _accessory;
+        private set
+        {
+            _accessory = value;
+
+            if (_accessory.GetType() == typeof(ExpBooster))
+                ProgressionType = ProgressionType.Boosted;
+        }
+    }
+
+    public Character Equip(Accessory accessory)
+    {
+        Accessory = accessory;
+        return this;
+    }
+
+    public ProgressionType ProgressionType { get; private set; } = progressionType;
+    public Xp Xp { get; private set; } = xp ?? 0;
+
+    public void Set(Xp xp) => Xp = xp;
+}
+
+public class Attribute(ProgressionType progressionType = ProgressionType.Standard, Xp? xp = null) : IProgressable
+{
+    public static readonly Xp MaxXp = 255;
+    public ProgressionType ProgressionType { get; } = progressionType;
+    public Xp Xp { get; private set; } = xp ?? 0;
+    public void Set(Xp xp) => Xp = xp;
+}
+
+public class XpService
+{
+    private readonly Dictionary<ProgressionType, Func<ProgressionRate>> _createProgressionRate = new()
+    {
+        { ProgressionType.Standard, () => new StandardRate() },
+        { ProgressionType.Boosted, () => new BoostedRate() }
+    };
+
+    public void Add(IProgressable progressable, Xp xp, Xp maxXp)
+    {
+        _createProgressionRate[progressable.ProgressionType]().Add(progressable, xp, maxXp);
+    }
+}
---
+++ character-progression/source/Domain3.Service/WhenAddingXp.cs
@@ -0,0 +1,113 @@
+﻿using FluentAssertions;
+
+namespace CharacterProgression.Domain3.Service;
+
+public abstract class WhenAddingXp(IProgressable progressable)
+{
+    #region Setup
+
+    private readonly XpService _xpService = new();
+
+    protected readonly IProgressable Progressable = progressable;
+
+    #endregion
+
+    #region Implementation
+
+    public abstract Xp GetMax();
+    public abstract void GivenMaxXp_ThenXpIsNotAdded();
+    public abstract void ThenXpIsClamped();
+
+    #endregion
+
+    #region Requirements
+
+    [Theory]
+    [InlineData(1, 5)]
+    [InlineData(100, 20)]
+    public void ThenXpIsAdded(ushort initial, ushort gained)
+    {
+        Progressable.Set(initial);
+
+        _xpService.Add(Progressable, gained, GetMax());
+
+        Progressable.Xp.Should().Be((ushort) (initial + gained));
+    }
+
+    #endregion
+
+    public class GivenAttribute() : WhenAddingXp(new Attribute())
+    {
+        #region Implementation
+
+        public override Xp GetMax() => Attribute.MaxXp;
+
+        #endregion
+
+        #region Requirements
+
+        [Fact]
+        public override void GivenMaxXp_ThenXpIsNotAdded()
+        {
+            Progressable.Set(Attribute.MaxXp);
+
+            _xpService.Add(Progressable, 20, Attribute.MaxXp);
+
+            Progressable.Xp.Should().Be(Attribute.MaxXp);
+        }
+
+        [Fact]
+        public override void ThenXpIsClamped()
+        {
+            _xpService.Add(Progressable, 300, Attribute.MaxXp);
+
+            Progressable.Xp.Should().Be(Attribute.MaxXp);
+        }
+
+        #endregion
+    }
+
+    public class GivenCharacter() : WhenAddingXp(new Character())
+    {
+        #region Implementation
+
+        public override Xp GetMax() => Character.MaxXp;
+
+        #endregion
+
+        #region Requirements
+
+        [Theory]
+        [InlineData(1, 5)]
+        [InlineData(100, 20)]
+        public void GivenBoostedRate_ThenXpIsClamped(ushort initial, ushort gained)
+        {
+            Progressable.Set(initial);
+            (Progressable as Character).Equip(new ExpBooster());
+
+            _xpService.Add(Progressable, gained, Character.MaxXp);
+
+            Progressable.Xp.Should().Be((ushort) (initial + gained * 2));
+        }
+
+        [Fact]
+        public override void GivenMaxXp_ThenXpIsNotAdded()
+        {
+            Progressable.Set(Character.MaxXp);
+
+            _xpService.Add(Progressable, 20, Character.MaxXp);
+
+            Progressable.Xp.Should().Be(Character.MaxXp);
+        }
+
+        [Fact]
+        public override void ThenXpIsClamped()
+        {
+            _xpService.Add(Progressable, 10000, Character.MaxXp);
+
+            Progressable.Xp.Should().Be(Character.MaxXp);
+        }
+
+        #endregion
+    }
+}
