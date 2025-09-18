7a1c58c9b750468d84443875ed024eab331c67ce
Jarrod Swords <jarrod.swords@gmail.com>
2025-09-18 03:13:00 -0400

    composition

---
+++ character-progression/source/Domain2.Composition/IProgressable.cs
@@ -0,0 +1,43 @@
+﻿namespace CharacterProgression.Domain2.Composition;
+
+public interface IProgressable
+{
+    Xp Xp { get; }
+    void Add(Xp xp);
+    void Set(Xp xp);
+}
+
+public class Progressable(Xp xp) : IProgressable
+{
+    public Xp Xp { get; private set; } = xp;
+
+    public void Add(Xp xp)
+    {
+        Xp += xp;
+    }
+
+    public void Set(Xp xp)
+    {
+        Xp = xp;
+    }
+}
+
+public class Character(Xp? xp = null) : IProgressable
+{
+    private readonly Progressable _progressable = new(xp ?? 0);
+
+    public Xp Xp => _progressable.Xp;
+
+    public void Add(Xp xp) => _progressable.Add(xp);
+    public void Set(Xp xp) => _progressable.Set(xp);
+}
+
+public class Attribute(Xp? xp = null) : IProgressable
+{
+    private readonly Progressable _progressable = new(xp ?? 0);
+
+    public Xp Xp => _progressable.Xp;
+
+    public void Add(Xp xp) => _progressable.Add(xp);
+    public void Set(Xp xp) => _progressable.Set(xp);
+}
---
+++ character-progression/source/Domain2.Composition/WhenAddingXp.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+
+namespace CharacterProgression.Domain2.Composition;
+
+public abstract class WhenAddingXp(IProgressable progressable)
+{
+    #region Setup
+
+    protected readonly IProgressable Progressable = progressable;
+
+    #endregion
+
+    #region Requirements
+
+    [Theory]
+    [InlineData(1, 5)]
+    [InlineData(100, 20)]
+    public void ThenXpIsAdded(ushort initial, ushort gained)
+    {
+        Progressable.Set(initial);
+
+        Progressable.Add(gained);
+
+        Progressable.Xp.Should().Be((ushort) (initial + gained));
+    }
+
+    #endregion
+
+    public class GivenAttribute() : WhenAddingXp(new Attribute())
+    {
+    }
+
+    public class GivenCharacter() : WhenAddingXp(new Character())
+    {
+    }
+}
